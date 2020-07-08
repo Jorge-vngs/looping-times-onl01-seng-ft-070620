f0a0f290b4bfdac77cd66303a2e5a25475c8c059
Jorge Venegas Martinez <brangelina01@gmail.com>
2020-07-08 08:38:42 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,12 +1,10 @@
 def using_times
 	7.times do 
 	  puts "Wingardium Leviosa"
+	end   
 end
 
 
 
 
 
-
-
-#Fill out the content of the using_times method so that calling it will puts the desired phrase, "Wingardium Leviosa" seven times by using the times keyword.
